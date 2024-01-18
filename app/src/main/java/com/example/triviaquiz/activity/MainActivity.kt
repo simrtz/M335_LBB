dc94860ae3dcff8368f4829cdd21330c3f355493
sru <simon.rutz@outlook.de>
2024-01-18 14:05:50 +0100

    Implement ResultActivity

--- app/src/main/java/com/example/triviaquiz/activity/MainActivity.kt
+++ app/src/main/java/com/example/triviaquiz/activity/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.triviaquiz.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import com.example.triviaquiz.R
@@ -12,13 +16,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setUpForm()
+        setUpButton()
+    }
+
+    private fun setUpForm() {
         val categorySelection = findViewById<Spinner>(R.id.category_dropdown)
         val difficultySelection = findViewById<Spinner>(R.id.difficulty_dropdown)
         val typeSelection = findViewById<Spinner>(R.id.question_type_dropdown)
 
-        val categoryAdapter = ArrayAdapter.createFromResource(this, R.array.categories, android.R.layout.simple_spinner_item)
-        val difficultyAdapter = ArrayAdapter.createFromResource(this, R.array.difficulties, android.R.layout.simple_spinner_item)
-        val typeAdapter = ArrayAdapter.createFromResource(this, R.array.question_types, android.R.layout.simple_spinner_item)
+        val categoryAdapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.categories,
+            android.R.layout.simple_spinner_item
+        )
+        val difficultyAdapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.difficulties,
+            android.R.layout.simple_spinner_item
+        )
+        val typeAdapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.question_types,
+            android.R.layout.simple_spinner_item
+        )
 
         categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         difficultyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
@@ -27,5 +48,24 @@ class MainActivity : AppCompatActivity() {
         categorySelection.adapter = categoryAdapter
         difficultySelection.adapter = difficultyAdapter
         typeSelection.adapter = typeAdapter
+    }
+
+    private fun setUpButton() {
+        val startButton = findViewById<Button>(R.id.start_button)
+
+        startButton.setOnClickListener { _ ->
+            val intent = Intent(this, ResultActivity::class.java)
+            val numberOfQuestions = findViewById<EditText>(R.id.number_of_question_text)
+            val category = findViewById<Spinner>(R.id.category_dropdown)
+            val difficulty = findViewById<Spinner>(R.id.difficulty_dropdown)
+            val type = findViewById<Spinner>(R.id.question_type_dropdown)
+
+            intent.putExtra("NUMBER_OF_QUESTIONS", numberOfQuestions.text.toString().toInt())
+            intent.putExtra("CATEGORY", category.selectedItem.toString())
+            intent.putExtra("DIFFICULTY", difficulty.selectedItem.toString())
+            intent.putExtra("CATEGORY", type.selectedItem.toString())
+
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/triviaquiz/activity/ResultActivity.kt
+++ app/src/main/java/com/example/triviaquiz/activity/ResultActivity.kt
@@ -1,15 +1,31 @@
 package com.example.triviaquiz.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.triviaquiz.R
+import com.example.triviaquiz.api.TriviaClient
+
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
-        val menuButton = findViewById<Button>(R.id.)
+        val correctlyAnswered = 0;
+        val numberOfQuestions = intent.getIntExtra("NUMBER_OF_QUESTIONS", 0)
+
+        val resultStatisticView = findViewById<TextView>(R.id.result_text)
+
+        val populatedResults = resources.getString(R.string.result_text, correctlyAnswered, numberOfQuestions)
+        resultStatisticView.text = populatedResults
+
+        val menuButton = findViewById<Button>(R.id.menu_button)
+        menuButton.setOnClickListener { _ ->
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
